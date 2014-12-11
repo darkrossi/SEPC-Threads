24318622873c4e3406f7635cd9a3e4102c637cec
hoel <hoel@AsusHoel.bouyguesbox.fr>
2014-12-11 19:24:13 +0100

    Commentaire de tsp-main-para.c

--- src/tsp-main-para.c
+++ src/tsp-main-para.c
@@ -16,16 +16,32 @@
 #include "tsp-print.h"
 #include "tsp-tsp.h"
 
+/***************************************************************
+ * Ce programme est multi-threads, fonctionne, mais est plus   *
+ * lent que la version initiale.                               *
+ * Principe du programme :                                     *
+ * On affecte un 'job' de la file d'attente à un thread        *
+ * disponible. Lorsqu'on voit qu'un thread est occupé, on      *
+ * teste le thread suivant et ainsi de suite.                  *
+ * Une fois la file vidée on attend la fin de tous les threads *
+ * lancés avant d'afficher le résultat.                        *
+ ***************************************************************/
+
 
 /* macro de mesure de temps, retourne une valeur en nanosecondes */
 #define TIME_DIFF(t1, t2) \
   ((t2.tv_sec - t1.tv_sec) * 1000000000ll + (long long int) (t2.tv_nsec - t1.tv_nsec))
 
-
+/* valeur de renvoi de la fonction tsp */
 extern void* ALL_IS_OK;
+
+/* fonction de recherche du meilleur chemin */
 extern void* tsp (void* arguments);
+
+/* mutex */
 extern pthread_mutex_t mutex;
 
+/* tableau contenant des informations sur les threads utilisés */
 Cell* tab_threads;
 
 /* tableau des distances */
@@ -43,8 +59,13 @@ int nb_threads=1;
 /* affichage SVG */
 bool affiche_sol= false;
 
+/* solution actuelle*/
 tsp_path_t sol;
+
+/* longueur de la solution actuelle*/
 int sol_len;
+
+/* nombre total de 'abort' lors du parcours des chemins */
 long long int cuts = 0;
 
 
@@ -114,62 +135,73 @@ int main (int argc, char **argv)
     init_queue (&q);
 
     clock_gettime (CLOCK_REALTIME, &t1);
-
+	
+	/* on vide le chemin 'path' : on met des -1 dans toutes les cases */
     memset (path, -1, MAX_TOWNS * sizeof (int));
+	/* on définit la première ville comme étant 0 */
     path[0] = 0;
 
-    /* mettre les travaux dans la file d'attente */
+    /* On met des travaux dans la file d'attente q */
     generate_tsp_jobs (&q, 1, 0, path, &cuts, sol, & sol_len, 3);
     no_more_jobs (&q);
    
-    /* calculer chacun des travaux */
+	/* on vide le chemin 'solution' : on met des -1 dans toutes les cases */
+	/* puis on définit la première ville comme étant 0 */
     tsp_path_t solution;
     memset (solution, -1, MAX_TOWNS * sizeof (int));
     solution[0] = 0;
 
+	/* allocation du tableau d'informations sur les threads */
     tab_threads = malloc(nb_threads*sizeof(Cell));
 	memset(tab_threads, 0, nb_threads*sizeof(int));
 
+	/* initialisation du mutex*/
 	pthread_mutex_init(&mutex, NULL);
 
 	int i;
 
+	/* on place tous les threads dans l'état 'non-occupé' */
 	for(i=0; i<nb_threads; i++){
 		tab_threads[i].occupe = 0;
     }
 
+	/* Tant que la file de jobs n'est pas vide on sort un job et on l'execute avec tsp */
     while (!empty_queue (&q)) {
         int hops = 0, len = 0;
+		/* on récupère un job dans la file d'atente des jobs */
         get_job (&q, solution, &hops, &len);
         
         i = 0;
+		/* on boucle tant qu'on n'a pas trouvé de thread pour effectuer le job */
         while (1){
+			/* si le thread i n'est pas occupé alors on va l'utiliser pour effectuer le job */
             if(!tab_threads[i].occupe){
                 
+				/* on place le thread i en statut 'occupé' */
                 tab_threads[i].occupe = 1;
 
+				/* on crée une structure arguments qui va recevoir les arguments de tsp */
                 struct arg_struct arguments;
 
 				arguments.hops = hops;
 				arguments.len = len;
 				memcpy(arguments.path, solution, nb_towns * sizeof (int));
 
-                arguments.pere = 1; // Un entier qui est 1 si le tsp appelé est celui du père et 0 si c'est dans les fils
+				/* tsp est récursive et on a besoin de savoir quand le thread a terminé le job afin de le mettre en 'non-occupé' */
+				/* On introduit donc un entier qui est 1 si le tsp appelé est celui du père et 0 si c'est dans les fils */
+                arguments.pere = 1; 
 
+				/* lancement du thread sur tsp avec les bons arguments*/
                 pthread_create (&tab_threads[i].thread, NULL, tsp, (void*)&arguments);
                 break;
             }
+			/* sinon on va regarder si le suivant%nb_threads est occupé */
             i = (i+1)%nb_threads;
         }
-
-		//solution [[provisoire]] pour la gestion de l'occupation des threads
-		// for(i=0; i<nb_threads; i++){
-  //       	pthread_join(tab_threads[i].thread, NULL);
-		// 	tab_threads[i].occupe = 0;
-  //   	}
     }
 
     i = 0;
+	/* on attend la fin de tous les threads avant de continuer */
     while(1){
         pthread_join(tab_threads[i].thread, NULL);
         i++;
